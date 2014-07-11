70371510d316b8e9df6bfc563baf565d5ad7ee3a
Colin Powell <colin.powell@gmail.com>
2014-07-11 15:34:53 -0400

    Fixing how workers are displaying in the sidear.

--- beehve/apps/honey/context_processors.py
+++ beehve/apps/honey/context_processors.py
@@ -1,10 +1,11 @@
 from django.template import RequestContext
 
 from .models import Project, Technology, Topic, Event
+from workers.models import Worker
 from django.contrib.auth import get_user_model
 
 def menu_preloader(request):
-    workers = get_user_model().objects.all()
+    workers = Worker.objects.all()
     projects = Project.objects.all()
     technologies = Technology.objects.all()
     topics = Topic.objects.all()
